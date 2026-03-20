0b70e8d20bf30b7707c6dd80abeced35fd0c47b0
photonest444 <maxim.4.kosmos@gmail.com>
2026-03-20 20:41:03 +0600

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use webview2_browser::*;
+use webview_official::*;
 
 fn main() {
-    let mut webview = WebView::new("FourMessenger", "https://fourmessenger.github.io");
-    webview.set_size(1200, 800);
+    let mut webview = WebviewBuilder::new()
+        .title("FourMessenger")
+        .width(1200)
+        .height(800)
+        .resize(SizeHint::NONE)
+        .debug(true)
+        .url("https://fourmessenger.github.io")
+        .build();
+
     webview.run();
 }
